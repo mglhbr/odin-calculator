c50bf5648b2c28c3720a33ad294ff101e5fdb0a3
mglhbr <120033327+mglhbr@users.noreply.github.com>
2023-02-25 13:43:51 +0100

    Add calculations if btn is clicked +display
    When you click the different operational buttons you can't click them
    again but the = button unlocks.
    When you click the = button, the operation is displayed and the solution
    is shown, = button is locked until new operation is chosen.

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,13 @@
 //Variables
 const buttons = document.querySelectorAll('.btn')
 let displayValue = 0;
+let displayOperation='';
+
 let firstNumber = 0;
 let secondNumber = 0;
+let currentOperator = '';
+let canOperate = 1; // 0=no 1=yes
+let canEquals = 0; // 0=no 1=yes
 
 
 //Operations
@@ -19,7 +24,7 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(operator, num1, num2){ //should be called after a operator button is pressed
+function operate(operator, num1, num2){ //should be called after = button is pressed
     if(operator==='add'){
         return add(num1, num2);
     }
@@ -45,9 +50,13 @@ function updateNumber(num) {
 //Button funcitons
 function clear(){
     document.getElementById('calcNum').innerHTML= 0
+    document.getElementById('calcOperation').innerHTML= ''
     displayValue = 0;
     firstNumber = 0;
     secondNumber = 0;
+    currentOperator = '';
+    canOperate = 1;
+
 }
 
 function deleteNumber(){
@@ -57,10 +66,51 @@ function deleteNumber(){
     document.getElementById('calcNum').innerHTML= displayValue
 }
 
-//if a operation button is pressed, num1 should be saved
+//if a operation button is pressed:
+// num1 saved, displayValue = 0 + HTML updated
+// id of button saved in variable (global)
+// num 1 + (operator) in calcOperation div
+function updateOperation(button){
+    if(canOperate===1){ // so you cant hit an operation 2x in a row
+        firstNumber = displayValue;
+        displayValue = 0;
+        document.getElementById('calcNum').innerHTML= displayValue
+        currentOperator = button.getAttribute('id') // saves the current operator so operate function can choose
+    
+        console.log(currentOperator)
+        //Operation is displayed and stored for further manipulation
+        displayOperation = firstNumber + ' ' + button.innerHTML
+        document.getElementById('calcOperation').innerHTML= displayOperation
+
+        canOperate=0;
+        canEquals=1;
+        return
+    }
+    else {return}
+}
+
+//if = button is pressed:
+//secondNumber should be saved & afterwards calculated with operate()
+//can operate again, bcs display Value is new num
+function equals(){
+    if(canEquals===1){
+        secondNumber = displayValue;
+        displayValue = operate(currentOperator, firstNumber, secondNumber);
+        document.getElementById('calcNum').innerHTML= displayValue
+    
+        //Operation is updated
+        displayOperation = displayOperation + ' '+ secondNumber
+        document.getElementById('calcOperation').innerHTML= displayOperation
+
+        canEquals=0;
+        canOperate=1;
+        return
+    }
+    else {return}// so you can only equals after you choose operator
+}
 
 
-//if = cutton is pressed, num2 should be saved & afterwards calculated
+//if dot(.) button is pressed:
 
 
 
@@ -77,9 +127,23 @@ function calculator(button) {
             deleteNumber();
             return
         }
+        else if(
+            button.getAttribute('id') === 'divide' ||
+            button.getAttribute('id') === 'multiply' ||
+            button.getAttribute('id') === 'subtract' ||
+            button.getAttribute('id') === 'add' 
+        ){
+            updateOperation(button);
+            return
+        }
+        else if (button.getAttribute('id') === 'equals'){
+            equals();
+
+        }
 
         return 
     }
+
     else { //should update the number
         return updateNumber(num)
     }
